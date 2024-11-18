9a372745d7b4dec2718e1a51d311881e104661be
Lucas Bonenfant <lucas@lynkz.ca>
2024-11-18 14:36:02 -0500

    Modification de custom_board pour spécifier les bonnes pins

--- TE_PB_1/inc/custom_board.h
+++ TE_PB_1/inc/custom_board.h
@@ -14,43 +14,43 @@ extern "C" {
 #endif
 
 //LEDS Default Pin Definition
-#define INT_STCO_LED 27 // Internal STARTCO LED
-#define INT_BV_LED 26 // Internal Bavard LED
-#define MUX1_UART_LED 25 // 1st UART selector LED
-#define MUX2_UART_LED 22 // 2st UART selector LED
-#define LOW_BAT_LED 14 // 2st UART selector LED
+#define INT_STCO_LED 29 // Internal STARTCO LED
+#define INT_BV_LED 30 // Internal Bavard LED
+#define MUX1_UART_LED 31 // 1st UART selector LED
+#define MUX2_UART_LED 10 // 2st UART selector LED
+#define LOW_BAT_LED 15 // 2st UART selector LED
 
 //UART Default Pin Definition
-#define UART_RX_PIN_NUMBER 23 // ProgBoard's ANNA UART Rx
-#define UART_TX_PIN_NUMBER 24 // ProgBoard's ANNA UART Tx
-#define TAG_RX_PIN_NUMBER 16 // TAG's ANNA UART Rx
-#define TAG_TX_PIN_NUMBER 15 // TAG's ANNA UART Tx
-#define BV_TX_PIN_NUMBER 13 // Bavard's UART Tx
+#define UART_RX_PIN_NUMBER 5 // ProgBoard's ANNA UART Rx
+#define UART_TX_PIN_NUMBER 4 // ProgBoard's ANNA UART Tx
+#define TAG_RX_PIN_NUMBER 18 // TAG's ANNA UART Rx
+#define TAG_TX_PIN_NUMBER 16 // TAG's ANNA UART Tx
+#define BV_TX_PIN_NUMBER 14 // Bavard's UART Tx
 #define SERIAL_RTS_PIN 0xFF
 #define SERIAL_CTS_PIN 0xFF.
 
 //Analog Switch Default Pin Definition
-#define SW1 37 // Analog switch to connect the red terminal to the TAG's STCO input
-#define SW2 36 // Analog switch to connect the red terminal to the Internal Bavard
-#define SW3 31 // Analog switch to connect the Internal STCO to the Internal Bavard
-#define SW4_5 30 // Analog switch to connect the black terminal to GND or the Internal STCO
-#define SW6 29 // Analog switch to connect Bavard's filter circuit when the Internal Bavard is used
+#define SW1 23 // Analog switch to connect the red terminal to the TAG's STCO input
+#define SW2 22 // Analog switch to connect the red terminal to the Internal Bavard
+#define SW3 26 // Analog switch to connect the Internal STCO to the Internal Bavard
+#define SW4_5 25 // Analog switch to connect the black terminal to GND or the Internal STCO
+#define SW6 27 // Analog switch to connect Bavard's filter circuit when the Internal Bavard is used
 
 //STARTCO Default Pin Definition
-#define STCO_OK 21 // HIGH if GND Check is passed
-#define STCO_OPEN_Z 45 // HIGH if GND Check says OPEN CIRCUIT
-#define STCO_SHORT_Z 38 // HIGH if GND Check says SHORT CIRCUIT
+#define STCO_OK 9 // HIGH if GND Check is passed
+#define STCO_OPEN_Z 20 // HIGH if GND Check says OPEN CIRCUIT
+#define STCO_SHORT_Z 24 // HIGH if GND Check says SHORT CIRCUIT
 
 //Buttons Default Pin Definition
-#define UART_SELECTOR_BTN 35 // Selector to chose which device communicate with UART through USB
-#define MODE_SELECTOR_BTN 34 // Selector to change the configuration (INTERNAL/EXTERNAL STCO/Bavard)
+#define UART_SELECTOR_BTN 19 // Selector to chose which device communicate with UART through USB
+#define MODE_SELECTOR_BTN 11 // Selector to change the configuration (INTERNAL/EXTERNAL STCO/Bavard)
 
 //TAG Power Default Pin Definition
 #define TAG_PWR 28 // Can power the TAG through this pin
-#define TAG_PWR_SENS 20 // Can read if the tag is powered
+#define TAG_PWR_SENS 2 // Can read if the tag is powered
 
 //Battery sens Default Pin Definition
-#define V_BAT_SENS 20 // Can read the battery's voltage
+#define V_BAT_SENS 3 // Can read the battery's voltage
 
 
 // Low frequency clock source to be used by the SoftDevice
